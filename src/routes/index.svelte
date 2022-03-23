734aa30b020fda3aa9cc6f30f45eca449d98840c
Doug A <douganderson444@gmail.com>
2022-03-23 12:23:51 -0300

    menu n cleanup

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,90 +1,48 @@
 <script>
 	import { panzoom } from '$lib/panzoom.js';
 	import { zoom } from '$lib/zoom.js';
-
 	import Spot from '$lib/_components/Spot.svelte';
 
 	let zoomable, container;
 	let style = ''; // show styling applied
-	let scale = {};
-	let matrix = {};
-	let origin;
+	let scale = { value: 1 };
+	let count = 10;
+	let min = count;
 
 	function handleZoom(e) {
-		console.log({ detail: e.detail });
+		console.log('Zoomed.', { detail: e.detail });
 		scale = e.detail.scale;
-		matrix = e.detail.matrix;
 		style = zoomable.style.transform;
-		origin = e.detail.origin;
 	}
-	let location = { x: 0, y: 0 };
-	function handleMouseMove(e) {
-		location = { x: e.clientX, y: e.clientY };
-	}
-	let count = 10;
+
 	const grid = Array.from({ length: count }, (_, i) =>
 		Array.from({ length: count }, (_, j) => ({ id: i * count + j }))
 	);
 </script>
 
-<div class="container" on:zoomed={handleZoom} on:mousemove={handleMouseMove} bind:this={container}>
-	<div style="position: absolute; left: 10px; z-index: 10;">Zoom Level: {scale.value}</div>
-	<div class="zoomable flexbox" bind:this={zoomable} use:zoom>
+<div class="container" bind:this={container}>
+	<div class="menu">
 		<div>
-			Scroll in me. <br />
-			{location?.x}, {location?.y}<br />
-			{origin?.x}, {origin?.y}
-			Style: {style}
+			Zoom Level: {scale.value}
 		</div>
+
+		<div>Style: {style}</div>
+	</div>
+	<div class="zoomable flexbox" bind:this={zoomable} use:zoom on:zoomed={handleZoom}>
 		{#if container}
 			<div class="grid">
 				{#each grid as col, x}
 					<div class="col">
 						{#each col as square, y}
 							<Spot
-								left={(x * container.offsetWidth) / count}
-								top={(y * container.offsetWidth) / count}
+								left={min + (x * container.offsetWidth) / count}
+								top={min + (y * container.offsetWidth) / count}
 							/>
 						{/each}
 					</div>
 				{/each}
 			</div>
 		{/if}
-
-		<!-- 
-		<div class="flexbox">
-			{#if scale}
-				<div class="flexitem">
-					Scale:
-					<ul>
-						{#each Object.entries(scale) as [k, v]}
-							<li>{k}: {v}</li>
-						{/each}
-					</ul>
-				</div>
-			{/if}
-			{#if matrix}
-				<div class="flexitem">
-					Matrix:
-					<ul>
-						{#each Object.entries(matrix) as [k, v]}
-							<li>{k}: {v}</li>
-						{/each}
-					</ul>
-				</div>
-			{/if}
-			{#if origin}
-				<div class="flexitem">
-					origin:
-					<ul>
-						{#each Object.entries(origin) as [k, v]}
-							<li>{k}: {v}</li>
-						{/each}
-					</ul>
-				</div>
-			{/if}
-		</div>
-		 -->
 	</div>
 </div>
 
@@ -126,5 +84,15 @@
 	}
 	ul {
 		padding: 1em;
+	}
+
+	.menu {
+		position: absolute;
+		top: 10px;
+		left: 10px;
+		margin: 0.1em;
+		padding: 2em;
+		z-index: 10;
+		background-color: rgba(133, 198, 255, 0.801);
 	}
 </style>
