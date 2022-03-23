6c15667c64303b556348a51db0e0d3ea364acc97
Doug A <douganderson444@gmail.com>
2022-03-23 12:24:12 -0300

    rm coords

--- src/lib/_components/Spot.svelte
+++ src/lib/_components/Spot.svelte
@@ -3,16 +3,19 @@
 	export let left = ((1 << range) * Math.random()) | 10;
 	export let top = ((1 << range) * Math.random()) | 10;
 
-	let color = '#' + (((1 << 24) * Math.random()) | 0).toString(16);
+	let color = '#' + (((1 << 24) * Math.random()) | 4095).toString(16);
 </script>
 
-<div style="top: {top}px; left: {left}px; --color: {color};">{left},{top}</div>
+<div style="top: {top}px; left: {left}px; --color: {color};">
+	<!-- {left},{top} -->
+</div>
 
 <style>
 	div {
 		--size: 20px;
 		position: absolute;
 		background-color: var(--color);
+		border: 1px solid #00000049;
 		width: var(--size);
 		height: var(--size);
 	}
